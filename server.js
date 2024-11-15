477dd3a3ab33169109fb4a5ec9437ff8dcbe65a5
Sonia Chavepeyre <ciarasonia013@gmail.com>
2024-11-15 13:28:55 +0100

    changement port version2

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const app = require('./app'); // Importation de l'application
 
 // Définir le port
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 console.log('Serveur démarré');
 // Démarrer le serveur
 if (require.main === module) {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  /*console.log(`Documentation Swagger disponible sur http://localhost:${PORT}/api-docs`);*/
+  console.log(`Documentation Swagger disponible sur http://localhost:${PORT}/api-docs`);
 });
 }
